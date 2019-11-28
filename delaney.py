2a178079203d1f296990e6ca335028c587468a28
Hunter <hunterjmery@gmail.com>
2019-11-27 22:54:10 -0700

    This code works

---
+++ delaney.py
@@ -0,0 +1,26 @@
+import requests
+import time
+
+img_url="https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/"
+countries = ['us', 'aa', 'ac', 'ae', 'af']
+urls = []
+filenames = []
+requestlist = []
+
+for i in range(len(countries)):
+	urls.append(img_url + countries[i] + '-lgflag.gif')
+	filenames.append(countries[i]+'lgflag.gif')
+
+for i in range(len(countries)):
+	requestlist.append(requests.get(urls[i]))
+
+r=requests.get("https://www.cia.gov/library/publications/resources/the-world-factbook/graphics/flags/large/us-lgflag.gif")
+
+#need to create list or dic to store values of flag and have it iterate over that list in order to download all gifs
+
+for i in range(len(countries)):
+	with open( filenames[i],"wb") as f:
+		f.write(requestlist[i].content)
+
+# for i in range(len(file)):
+print("Total time elapsed: ",time.perf_counter ())
